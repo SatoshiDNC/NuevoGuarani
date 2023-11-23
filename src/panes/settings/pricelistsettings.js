7222429261271ff4c0fa90ed4c8092aac56a26a9
SatoshiDNC <octocat@satoshidnc.com>
2023-11-23 13:26:28 -0300

    add stall selector

--- src/panes/settings/pricelistsettings.js
+++ src/panes/settings/pricelistsettings.js
@@ -1,5 +1,92 @@
 var pricelisttypes = ['none', 'manual', 'NostrMarket compatible'];
 
+
+const nostrmarketstall = v = new vp.View(null);
+v.name = Object.keys({nostrmarketstall}).pop();
+v.title = 'stall';
+v.minX = 0; v.maxX = 0;
+v.minY = 0; v.maxY = 0;
+v.gadgets.push(v.swipeGad = new vp.SwipeGadget(v));
+v.swipeGad.actionFlags = vp.GAF_SWIPEABLE_UPDOWN | vp.GAF_SCROLLABLE_UPDOWN;
+v.swipeGad.hide = true;
+v.gadgets.push(v.list = g = new vp.Gadget(v));
+	g.key = 'nostrMarketStall';
+	Object.defineProperty(g, "list", {
+		get : function () {
+			return ['tbd'];
+		}
+	});
+  g.index = -1;
+	Object.defineProperty(g, "value", {
+		get : function () {
+			if (this.index >= 0 && this.index < this.list.length)
+				return this.list[this.index];
+			return '';
+		}
+	});
+	g.appFunction = function() {
+		pricelistsettings.queueLayout();
+	}
+	g.listItemClick = function(index) {
+		const g = this;
+		{ // For the GUI.
+			g.index = index; v.setRenderFlag(true);
+		} { // For the app function.
+			g.appFunction();
+		} { // For persistence.
+			var req = db.transaction(["settings"], "readwrite");
+			req.objectStore("settings")
+				.put(g.value,
+					`${getCurrentAccount().id}-${g.key}`);
+			req.onsuccess = (event) => {
+				console.log(`successfully selected ${g.key}`, event);
+			};
+			req.onerror = (event) => {
+				console.log(`error selecting ${g.key}`, event);
+			};
+		}
+	}
+v.load = function(cb) {
+	const debuglog = false;
+	const g = this.list;
+	g.tempValue = g.defaultValue;
+	function finishInit(cb, v, g) {
+		var index = -1;
+		for (var i=0; i<g.list.length; i++) {
+			if (g.list[i] == g.tempValue) {
+				index = i;
+				break;
+			}
+		}
+		if (index < 0) index = 0;
+		{ // For the GUI.
+			nostrmarketstall.list.index = index;
+			nostrmarketstall.setRenderFlag(true);
+		} { // For the app function.
+			g.appFunction();
+		} { // For persistence.
+		}
+		delete g.tempValue;
+		if (debuglog) console.log(`${g.key} ready`, g.value);
+		v.loadComplete = true; cb();
+	}
+	if (debuglog) console.log("requesting", `${getCurrentAccount().id}-${g.key}`);
+	var req = db.transaction(["settings"], "readonly")
+		.objectStore("settings")
+		.get(`${getCurrentAccount().id}-${g.key}`);
+	req.onsuccess = (event) => {
+		if (event.target.result !== undefined)
+			g.tempValue = event.target.result;
+		if (debuglog) console.log(`${g.key} restored`, g.tempValue);
+		finishInit(cb, this, g);
+	};
+	req.onerror = (event) => {
+		console.log(`error getting ${g.key}`, event);
+		finishInit(cb, this, g);
+	};
+}
+
+
 const pricelistsettings = v = new vp.View(null);
 v.name = Object.keys({pricelistsettings}).pop();
 v.title = 'price list';
@@ -33,9 +120,11 @@ v.gadgets.push(v.typelist = g = new vp.Gadget(v));
 			if (pricelisttypes[index] == 'NostrMarket compatible') {
 				delete v.nostrmarketurl.hide;
 				delete v.nostrmarketwalletkey.hide;
+				delete v.nostrmarketstall.hide;
 			} else {
 				v.nostrmarketurl.hide = true;
 				v.nostrmarketwalletkey.hide = true;
+				v.nostrmarketstall.hide = true;
 			}
 
 /*
@@ -144,6 +233,12 @@ v.gadgets.push(v.nostrmarketwalletkey = g = new vp.Gadget(v));
 			};
 		}
 	}
+  v.gadgets.push(v.nostrmarketstall = g = new vp.Gadget(v));
+	g.title = 'stall';
+	Object.defineProperty(g, "subtitle", {
+		get : function () { try { return nostrmarketstall.list.value; } catch (e) {} }
+	});
+	g.pane = nostrmarketstall;
 /*
 v.gadgets.push(v.strikeurl = g = new vp.Gadget(v));
 	g.type = 'button';
