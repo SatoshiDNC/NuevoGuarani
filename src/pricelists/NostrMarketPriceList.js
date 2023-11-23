fa3224928e100ac3f0b2689938877e0955adb1c2
SatoshiDNC <octocat@satoshidnc.com>
2023-11-23 18:03:17 -0300

    start framework for loading price data

--- src/pricelists/NostrMarketPriceList.js
+++ src/pricelists/NostrMarketPriceList.js
@@ -1,11 +1,15 @@
 class NostrMarketPriceList extends PriceList {
-	constructor() {
+  static list = []
+  constructor() {
 		super();
-    this.list = [
+ 	}
+
+  static loadData() {
+    list = [
       { name: 'Product XYZ', cur: '₲', amt: 12000, qty: 1, unit: 'ea' },
       { name: 'Product UVW', cur: '₲', amt: 22000, qty: 1000, unit: 'g' },
     ]
-	}
+  }
 
 	count() {
     return this.list.length;
