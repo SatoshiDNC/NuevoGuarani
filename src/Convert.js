7ab8775d12c4e7f4ae9cb866a6046b740acf54cd
SatoshiDNC <octocat@satoshidnc.com>
2023-11-23 19:08:47 -0300

    add converter

---
+++ src/Convert.js
@@ -0,0 +1,19 @@
+class Convert {
+	constructor() {}
+  static currencies = [
+    { app: '₿', LNbits: 'BTC' },
+    { app: '₲', LNbits: 'PYG' },
+    { app: '$', LNbits: 'USD' },
+    { app: '€', LNbits: 'EUR' },
+]
+  static LNbitsCurrencyToAppCurrency(input) {
+    for (c of Convert.currencies) {
+      if (c.LNbits == input) return c.app
+    }
+  }
+  static AppCurrencyToLNbitsCurrency(input) {
+    for (c of Convert.currencies) {
+      if (c.app == input) return c.LNbits
+    }
+  }
+}
