f03b891103b8ff8e3737890b48a79815618640df
SatoshiDNC <octocat@satoshidnc.com>
2023-11-23 18:37:10 -0300

    find stall by name

--- src/pricelists/NostrMarketPriceList.js
+++ src/pricelists/NostrMarketPriceList.js
@@ -5,11 +5,29 @@ class NostrMarketPriceList extends PriceList {
  	}
 
   static loadData(url, key, stall) {
-    console.log(url, key, stall)
-    NostrMarketPriceList.list = [
-      { name: 'Product XYZ', cur: '₲', amt: 12000, qty: 1, unit: 'ea' },
-      { name: 'Product UVW', cur: '₲', amt: 22000, qty: 1000, unit: 'g' },
-    ]
+    console.group(this.constructor.name+'(', url, key, stall, ')')
+    console.log('searching for stall', key != '')
+    const asyncLogic = async () => {
+      console.log('getting stalls', url)
+      const response = await fetch(url+'/stall?pending=false&api-key='+key, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+        },
+      });
+      const json = await response.json()
+      console.log(json)
+      const stallId = ''
+      json.map(e => { const { id, name } = e; if (name == stall) stallId = id })
+      console.log(stallId)
+
+      NostrMarketPriceList.list = [
+        { name: 'Product XYZ', cur: '₲', amt: 12000, qty: 1, unit: 'ea' },
+        { name: 'Product UVW', cur: '₲', amt: 22000, qty: 1000, unit: 'g' },
+      ]
+    }
+    asyncLogic()
+    console.groupEnd()
   }
 
 	count() {
