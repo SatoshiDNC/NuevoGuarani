7690b28751b0cb1dc33c066f493ffc6995bd5549
SatoshiDNC <octocat@satoshidnc.com>
2023-11-27 17:59:58 -0300

    debug

--- src/main-shapes.js
+++ src/main-shapes.js
@@ -191,7 +191,7 @@ const mainShapes = new ShapeBuffer(function() {
 })
 
 const emojiShapes = new ShapeBuffer(function(obj, emojiData, emojisPerRow, emojisPerColumn) {
-
+console.log(emojiData)
   // Specialized shape-building function for emoji textures
 	function addEmojiShape4(obj, name, typ, x, y) {
 		const nx = emojisPerRow, ny = emojisPerColumn
