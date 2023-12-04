850a4d77e51397e102ea398c7e3e4a16fcea764e
SatoshiDNC <octocat@satoshidnc.com>
2023-12-04 11:59:10 -0300

    debug

--- src/PriceList.js
+++ src/PriceList.js
@@ -230,6 +230,7 @@ class PriceList {
         this._emojiBase = Math.ceil(Math.sqrt(json.length))
         this.rows = this._emojiBase
         this.cols = this._emojiBase
+        console.log(this)
         let xIter = 0, yIter = 0
         json.map(e => {
           const { id, name, price } = e
@@ -244,7 +245,9 @@ class PriceList {
             yIter += 1
           }
         })
+        console.log(this)
         for (const item of tempList) {
+          console.log(this)
           this.addPriceData(item.id, item.name, item.cur, item.amt, item.size, item.unit)
         }
       }
