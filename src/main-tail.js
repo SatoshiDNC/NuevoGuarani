c6d2a6644144bbacdec85327883f0e12add86935
SatoshiDNC <octocat@satoshidnc.com>
2023-12-04 19:45:57 -0300

    refine logic

--- src/main-tail.js
+++ src/main-tail.js
@@ -1,11 +1,10 @@
-  (function loop() {
-    setTimeout(() => {
-      console.log('beep')
-      vp.beep()
-    
-      loop()
-    }, 60000)
-  })()
+
+  function checkOnlineOrders() {
+    console.log('beep')
+    vp.beep()
+    setTimeout(checkOnlineOrders, 60000)
+  }
+  setTimeout(checkOnlineOrders, 60000)
 
   if (window.devicePixelRatio > 1) defaultFont.fidelity = Math.max(1, 4 - Math.floor(window.devicePixelRatio));
 
