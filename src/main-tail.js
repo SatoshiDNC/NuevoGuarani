049645a587052d5dff942c595b7575bae2a629eb
SatoshiDNC <octocat@satoshidnc.com>
2023-12-05 08:15:37 -0300

    isolate scopes

--- src/main-tail.js
+++ src/main-tail.js
@@ -21,45 +21,48 @@
             if (event.target.result === undefined) {
               console.log('new order:', o)
 
-              var currentState = 'saved'
-              let subtotal = 0
-              const newOrder = {
-                nostrmarketId: id,
-                store: getCurrentAccount().id,
-                status: currentState,
-                date: new Date(),
-                "dataentry": {
-                  textbox: '',
-                  options: {},
-                },
-                currency: Convert.LNbitsCurrencyToAppCurrency(o.extra.currency),
-                items: o.items.map(item => {
-                  const unitprice = o.extra.products.filter(p => p.id === item.id)[0].price
-                  const qty = item.quantity
-                  subtotal += unitprice * qty
-                  return { qty:+qty, unitprice:+unitprice, currency:o.extra.currency, options:{} }
-                }),
-                subtotal: subtotal,
-                //amountTendered: receivepayment.cash.text,
-                //amountToReturn: returnchange.change.text,
-              };
-              // if (JSON.stringify(billpane.conversions) != '{}') {
-              //   newItem.conversions = billpane.conversions
-              // }
-              console.log('Saving', newOrder)
-              const tx = db.transaction(["sales"], "readwrite")
-              tx.onerror = (event) => { console.log("Save transaction failed.") }
-              tx.oncomplete = (event) => { }
-              const req = tx.objectStore("sales").add(newOrder)
-              req.onerror = (event) => { console.log("Save request failed.") }
-              req.onsuccess = (event) => { }
-              
-              
-              var req = db.transaction(["nostrmarket-orders"], "readwrite")
-                .objectStore("nostrmarket-orders")
-                .add({ id }, `${getCurrentAccount().id}-${id}`)
-              req.onerror = (event) => { console.log("DB write error.") }
-              req.onsuccess = (event) => { }
+              {
+                var currentState = 'saved'
+                let subtotal = 0
+                const newOrder = {
+                  nostrmarketId: id,
+                  store: getCurrentAccount().id,
+                  status: currentState,
+                  date: new Date(),
+                  "dataentry": {
+                    textbox: '',
+                    options: {},
+                  },
+                  currency: Convert.LNbitsCurrencyToAppCurrency(o.extra.currency),
+                  items: o.items.map(item => {
+                    const unitprice = o.extra.products.filter(p => p.id === item.id)[0].price
+                    const qty = item.quantity
+                    subtotal += unitprice * qty
+                    return { qty:+qty, unitprice:+unitprice, currency:o.extra.currency, options:{} }
+                  }),
+                  subtotal: subtotal,
+                  //amountTendered: receivepayment.cash.text,
+                  //amountToReturn: returnchange.change.text,
+                };
+                // if (JSON.stringify(billpane.conversions) != '{}') {
+                //   newItem.conversions = billpane.conversions
+                // }
+                console.log('Saving', newOrder)
+                const tx = db.transaction(["sales"], "readwrite")
+                tx.onerror = (event) => { console.log("Save transaction failed.") }
+                tx.oncomplete = (event) => { }
+                const req = tx.objectStore("sales").add(newOrder)
+                req.onerror = (event) => { console.log("Save request failed.") }
+                req.onsuccess = (event) => { }
+              }
+
+              {
+                var req = db.transaction(["nostrmarket-orders"], "readwrite")
+                  .objectStore("nostrmarket-orders")
+                  .add({ id }, `${getCurrentAccount().id}-${id}`)
+                req.onerror = (event) => { console.log("DB write error.") }
+                req.onsuccess = (event) => { }
+              }
             }
           }
       
