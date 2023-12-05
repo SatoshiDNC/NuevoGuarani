2c704fa5973807b20aaa8fae1eb2506cde46fa4a
SatoshiDNC <octocat@satoshidnc.com>
2023-12-05 07:19:29 -0300

    introduce new db version

--- src/main-tail.js
+++ src/main-tail.js
@@ -34,7 +34,7 @@
     asyncLogic()
     setTimeout(checkOnlineOrders, 60000)
   }
-  setTimeout(checkOnlineOrders, 60000)
+  setTimeout(checkOnlineOrders, 6000)
 
   if (window.devicePixelRatio > 1) defaultFont.fidelity = Math.max(1, 4 - Math.floor(window.devicePixelRatio));
 
