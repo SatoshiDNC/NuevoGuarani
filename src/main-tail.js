e15b531e2950fbfcaef525bea08f3d6ffd949cff
SatoshiDNC <octocat@satoshidnc.com>
2023-12-05 06:27:36 -0300

    implement order check

--- src/main-tail.js
+++ src/main-tail.js
@@ -1,7 +1,27 @@
 
   function checkOnlineOrders() {
-    console.log(config.stallKeys)
-    vp.beep()
+    const asyncLogic = async () => {
+      let stall, stallCurrency
+      {
+        console.log('checking for orders')
+        const response = await fetch(config.stallKeys.url+'/stall/order/'+config.stallKeys.stall.id+'?api-key='+config.stallKeys.key, {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json',
+          },
+        });
+        const json = await response.json()
+        console.log(json)
+        // json.map(e => {
+        //   const { id, name, currency } = e
+        //   if (id == stallId) {
+        //     stall = name
+        //     stallCurrency = currency
+        //   }
+        // })
+      }
+    }
+    asyncLogic()
     setTimeout(checkOnlineOrders, 60000)
   }
   setTimeout(checkOnlineOrders, 60000)
