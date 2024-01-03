e70bcc4ffd0756877629959f9d4cbc1bbf4e8c2d
Satoshi <octocat@satoshidnc.com>
2024-01-03 08:11:13 -0300

    handle case when no camera is selected

--- web/src/panes/settings/camerasettings.js
+++ web/src/panes/settings/camerasettings.js
@@ -34,12 +34,14 @@ function loadCameraSettingGuarded() {
 	var selectedCameraId = { index: -1, data: {} };
 	function finishCameraInit() {
 		var index = -1;
-		for (var i=0; i<cameras.length; i++) {
-			if (cameras[i].deviceId == selectedCameraId.data.deviceId) {
-				index = i;
-				break;
-			}
-		}
+    if (selectedCameraId.index !== -1) {
+      for (var i=0; i<cameras.length; i++) {
+        if (cameras[i].deviceId == selectedCameraId.data.deviceId) {
+          index = i;
+          break;
+        }
+      }
+    }
     if (index == -1) {
       for (var i=0; i<cameras.length; i++) {
         if (i == selectedCameraId.index) {
