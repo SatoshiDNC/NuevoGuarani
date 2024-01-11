5bcbafb9560e628f3705d67a81681d8116c85d01
Satoshi <octocat@satoshidnc.com>
2024-01-11 03:01:20 -0300

    full name reference

--- web/src/panes/settings/wallets/invoicepayments.js
+++ web/src/panes/settings/wallets/invoicepayments.js
@@ -3,7 +3,7 @@
 const invoicepaymentswalletsettings = v = new vp.View(null);
 v.name = Object.keys({invoicepaymentswalletsettings}).pop();
 v.title = 'invoice payments';
-v.wallettypes = salesincome.wallettypes
+v.wallettypes = salesincomewalletsettings.wallettypes
 v.minX = 0; v.maxX = 0;
 v.minY = 0; v.maxY = 0;
 v.gadgets.push(v.swipeGad = new vp.SwipeGadget(v));
