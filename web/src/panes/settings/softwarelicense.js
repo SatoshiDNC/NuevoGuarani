30c2143e71686ff70827c6821db51a808b1d6029
Satoshi <octocat@satoshidnc.com>
2024-01-12 17:39:29 -0300

    draw spinner

--- web/src/panes/settings/softwarelicense.js
+++ web/src/panes/settings/softwarelicense.js
@@ -60,6 +60,14 @@ v.gadgets.push(v.spinner = g = new vp.Gadget(v))
   g.renderFunc = function() {
     const g = this, v = g.viewport
     console.log('render')
+
+		this.busyCounter += 0.01; if (this.busyCounter > Math.PI/2) this.busyCounter -= Math.PI/2
+		mat4.identity(m)
+		mat4.translate(m,m,[g.x+g.w/2,g.y+g.h/2,0])
+		//mat4.scale(m,m,[w,w,1]);
+		mat4.rotate(m,m, this.busyCounter, [0,0,1])
+		iconFont.draw(-10,7,"\x0A",config.themeColors.uiText,this.mat, m)
+    
     v.setRenderFlag(true)
   }
 v.gadgets.push(v.license = g = new vp.Gadget(v))
