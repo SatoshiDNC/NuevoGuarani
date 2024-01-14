f738df6091bb5348404e2fc09d1bf8fc764a6c10
Satoshi <octocat@satoshidnc.com>
2024-01-14 10:25:00 -0300

    tweak

--- web/src/panes/settings/mainsettings.js
+++ web/src/panes/settings/mainsettings.js
@@ -250,8 +250,8 @@ v.renderFunc = function() {
 				gl.uniform4fv(gl.getUniformLocation(prog6, 'overallColor'),
 					new Float32Array([1,1,1,1]))
 				mat4.identity(mat)
-				mat4.translate(mat,mat, [g.x+margin+25,g.y,0])
-				mat4.scale(mat,mat, [g.w-2*margin-50,1,1])
+				mat4.translate(mat,mat, [g.x,g.y,0])
+				mat4.scale(mat,mat, [g.w,1,1])
 				gl.uniformMatrix4fv(gl.getUniformLocation(prog6, 'uModelViewMatrix'), false, mat)
 				mainShapes.drawArrays6('divSettings')
 			}
