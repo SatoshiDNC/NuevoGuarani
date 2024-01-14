fdfa3a06cbab83c8db9a5c3df04c185a467df28a
Satoshi <octocat@satoshidnc.com>
2024-01-14 00:45:12 -0300

    strange

--- web/src/panes/settings/softwarelicense.js
+++ web/src/panes/settings/softwarelicense.js
@@ -187,7 +187,7 @@ v.gadgets.push(v.confirmamount = g = new vp.Gadget(v));
 		get : function () {
 			if (this.value) {
 				var temp = this.value
-				if (+temp <= 0) return 'not set'
+				if (temp == '') return 'not set'
 				else return ''+temp
       }
 		}
