4ce220646cc285b6b49df420c747cabdaec76ee3
Satoshi <octocat@satoshidnc.com>
2024-01-19 23:18:44 -0300

    handle unicode better

--- web/src/Convert.js
+++ web/src/Convert.js
@@ -99,7 +99,7 @@ class Convert {
   }
 
   static StringToDataURL(stringData, mimeType) {
-    return `data:${mimeType};base64,${btoa(new TextEncoder().encode(stringData))}`
+    return `data:${mimeType};base64,${btoa(String.fromCodePoint(...new TextEncoder().encode(stringData)))}`
   }
 
   static MaskIfGadgetIsSensitive(g, value) {
