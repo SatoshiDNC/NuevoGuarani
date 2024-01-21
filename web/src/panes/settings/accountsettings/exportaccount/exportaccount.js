c6ee9dff03f448089180eda578f9f129eaf57e49
Satoshi <octocat@satoshidnc.com>
2024-01-20 23:58:13 -0300

    debug

--- web/src/panes/settings/accountsettings/exportaccount/exportaccount.js
+++ web/src/panes/settings/accountsettings/exportaccount/exportaccount.js
@@ -70,7 +70,7 @@ v.gadgets.push(v.export = g = new vp.Gadget(v))
       let payload = JSON.stringify(data)
       v.qrcode.busySignal = true
       v.qrcode.hide = false
-      v.qrcode.qr = [payload]
+      v.qrcode.qr = ['test']
       v.queueLayout()
     }
 
