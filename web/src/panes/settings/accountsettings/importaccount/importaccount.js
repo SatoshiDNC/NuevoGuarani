d95edac2635a1eabefb19b6312963480f38bda23
Satoshi <octocat@satoshidnc.com>
2024-01-21 21:39:53 -0300

    debug

--- web/src/panes/settings/accountsettings/importaccount/importaccount.js
+++ web/src/panes/settings/accountsettings/importaccount/importaccount.js
@@ -474,7 +474,7 @@ v.renderFuncAux = function() {
 		//mat4.scale(m, m, [w/v.videoDims[0], h/v.videoDims[1], 1])
 		//mat4.translate(m, m, [x, y, 0])
 		//mat4.translate(m, m, [0.43, 0.43 + 0.14, 0])
-		mat4.scale(m, m, [0.14, -0.14, 1])
+		//mat4.scale(m, m, [0.14, -0.14, 1])
 		gl.uniformMatrix4fv(gl.getUniformLocation(prog2, 'uModelViewMatrix'), false, m)
 		var tris = mainShapes.len2.pies/3
 		var parts = this.scanner.results.length
