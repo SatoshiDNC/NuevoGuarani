94a6888b71c4ab881b3e01a8b5814179ca8f2fb7
Satoshi <octocat@satoshidnc.com>
2024-01-21 22:33:11 -0300

    debug

--- web/src/panes/settings/accountsettings/importaccount/importaccount.js
+++ web/src/panes/settings/accountsettings/importaccount/importaccount.js
@@ -459,9 +459,9 @@ v.renderFuncAux = function() {
 		var p = this.scanner.lastresult.cornerPoints
     x = -v.videoDims[0]/2
     y = -v.videoDims[1]/2
-    let xo = g.w/2
-    let yo = g.h/2
-    const vs = g.w / Math.min(v.videoDims[0], v.videoDims[1])
+    let xo = 0// g.w/2
+    let yo = 0//g.h/2
+    const vs = 1//g.w / Math.min(v.videoDims[0], v.videoDims[1])
 		var t = transform2d(undefined,
 			(p[0].x+x)*vs+xo, (p[0].y+y)*vs+yo, (p[1].x+x)*vs+xo, (p[1].y+y)*vs+yo, (p[3].x+x)*vs+xo, (p[3].y+y)*vs+yo, (p[2].x+x)*vs+xo, (p[2].y+y))*vs+yo
 		let m = mat4.fromValues(
